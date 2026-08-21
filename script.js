5654359a2dc038b4a2699f7d6d38b2db961b6805
CqwqY <1784627101@qq.com>
2026-08-21 17:14:39 +0800

    Update API_BASE URL in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ========== 配置 ==========
-const API_BASE = 'http://lshserver.dpdns.org:3000';
+const API_BASE = 'http://wuziqi.lshserver.dpdns.org';
 const POLL_INTERVAL = 500;
 let authToken = localStorage.getItem('token') || '';
 let currentUser = null;
